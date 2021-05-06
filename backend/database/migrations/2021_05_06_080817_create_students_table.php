5f95e698f9bb24e3b79e1869b06cfb066a0b0880
AngelMiguel2020 <73338230+AngelMiguel2020@users.noreply.github.com>
2021-05-06 10:46:19 +0200

    bd student

---
+++ backend/database/migrations/2021_05_06_080817_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('nif',  9);
+            $table->string('last_name1',  255);
+            $table->string('last_name2',  255);
+            $table->date('date_birth');
+            $table->integer('mobile_number',  9);
+            $table->string('photo_path',  255);
+            $table->enum('enrolment_status', ['MATRICULADO', 'BORRADOR', 'PREMATRICULADO', 'RECHAZADO']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('students');
+    }
+}
