50307864a508b2cdeba0b0464cf902f0f8e912ed
a18jorcalari <a18jorcalari@inspedralbes.cat>
2021-05-10 23:17:45 +0200

    Update CheckboxGroup.jsx

--- frontend/src/App/Enrolment/components/CheckboxGroup.jsx
+++ frontend/src/App/Enrolment/components/CheckboxGroup.jsx
@@ -20,41 +20,41 @@ function CheckboxGroup(props) {
           console.log("checkbox field", field);
           // console.log("checkbox field.value", field.value);
 
-          // return (
-          // <FormControl>
-          //   <FormGroup row={true}>
-          return options.map((option) => {
-            // console.log("checkbox option", option);
-            return (
-              <React.Fragment key={option.key}>
-                <input
+          return (
+            <FormControl>
+              <FormGroup row={true}>
+                {options.map((option) => {
+                  // console.log("checkbox option", option);
+                  return (
+                    <React.Fragment key={option.key}>
+                      {/* <input
                   type="checkbox"
                   id={option.key}
                   {...field}
                   {...rest}
-                  value={option.value}
-                  // checked={field.value.includes(option.value)}
-                  // checked={field.value.indexOf(option.value) !== -1}
-                />
-                <label htmlFor={option.key}>{option.value}</label>
+                  value={option.value} */}
+                      {/* // checked={field.value.includes(option.value)}
+                  // checked={field.value.indexOf(option.value) !== -1
+                  />
+                 <label htmlFor={option.key}>{option.value}</label> */}
 
-                {/* <FormControlLabel
+                      <FormControlLabel
                         control={
                           <Checkbox
-                            checked={field.value.includes(option.value)}
+                            // checked={field.value.includes(option.value)}
                             {...field}
                             {...rest}
                             color="primary"
                           />
                         }
                         label={option.value}
-                      /> */}
-              </React.Fragment>
-            );
-          });
-          //   </FormGroup>
-          // </FormControl>
-          // );
+                      />
+                    </React.Fragment>
+                  );
+                })}
+              </FormGroup>
+            </FormControl>
+          );
         }}
       </Field>
       <ErrorMessage component={TextError} name={name} />
