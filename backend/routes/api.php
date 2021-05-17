9d7e4db465fd18d14cc9456d70495a80475256e2
PabloGomezB <a18pabgombra@inspedralbes.cat>
2021-05-17 10:15:02 +0200

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,7 @@ use App\Models\Module;
 use App\Models\Student;
 
 use App\Http\Controllers\API\ApiController;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,3 +38,24 @@ Route::get('student/{nif}', [ApiController::class, function($nif){
 }]);
 
 Route::post('enrolments/add', [ApiController::class, 'addEnrolment']);
+
+Route::get('courses/{id}/modules',[ApiController::class, function($id){
+
+    // $courseInfo = DB::table('modules')->where('id_course', $id)->get();
+
+    // $courseInfo = DB::table('u_f_s')
+    //     ->join('modules', 'u_f_s.id_module', '=', 'modules.id')
+    //     ->where('u_f_s.id_module', $id)
+    //     ->select('u_f_s.*', 'modules.*')
+    //     ->get();
+
+
+    $courseInfo = DB::table('u_f_s')
+        ->join('modules', 'u_f_s.id_module', '=', 'modules.id')
+        ->where('u_f_s.id_module', $id)
+        // ->select('u_f_s.*', 'modules.*')
+        ->get();
+
+        // select u_f_s.*, modules.* from u_f_s INNER JOIN modules on u_f_s.id_module = modules.id where u_f_s.id_module=1
+    return $courseInfo;
+}]);
