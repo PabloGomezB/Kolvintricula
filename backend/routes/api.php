8dd98ca16c6e09781dda70822ccefb4efd625e79
SnowMan3sixty <73338167+SnowMan3sixty@users.noreply.github.com>
2021-05-19 20:39:49 +0200

    the api returns the correct json in /courses/{id}/modules
    databse modification is needed to use where and group by in the same sql query

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -41,33 +41,40 @@ Route::post('enrolments/add', [ApiController::class, 'addEnrolment']);
 
 Route::get('courses/{id}/modules',[ApiController::class, function($id){ 
 
-    $jsonUfs = DB::table('u_f_s')
-        ->join('modules', 'u_f_s.id_module', '=', 'modules.id')
-        ->where('modules.id_course', $id)
-        ->get(['modules.name','modules.description','u_f_s.name','u_f_s.description','u_f_s.year','u_f_s.id_module']);
+    $jsonFinal = array();
 
-    $jsonModules = DB::table('modules')
-        ->where('modules.id_course',$id)
-        ->get(['modules.id','modules.name','modules.description']); 
-    
-    // $jsonModules = $jsonModules->toArray();
-    // array_push($jsonModules,array("ufs"=>"asdasdasdasd"));
-    
-    $jsonModules = json_decode($jsonModules, TRUE);
-    for($i = 0; $i < sizeof($jsonModules); $i++) {
-        $jsonModules[$i]['ufs']=$jsonUfs;
+    //set the total number of courses in this loop
+    //this loop finds all the modules of the selected course in each year
+    for ($i=1; $i <= 2; $i++) { 
+
+        $jsonModules = DB::table('modules')
+            ->join('u_f_s','u_f_s.id_module','=','modules.id')
+            ->where('modules.id_course',$id)
+            ->where('u_f_s.year', $i)            
+            ->groupBy('modules.id')
+            ->get(['modules.id','modules.name','modules.description']);
+              
+
+        $jsonModules = json_decode($jsonModules, TRUE);
+
+        //this loop finds all the ufs in each module of the actual year
+        for($j = 0; $j < sizeof($jsonModules); $j++) {
+            
+            $jsonUfs = DB::table('u_f_s')
+                ->where('u_f_s.year',$i)
+                ->where('u_f_s.id_module',$jsonModules[$j]['id'])
+                ->get(['u_f_s.name','u_f_s.description','u_f_s.year','u_f_s.id_module']);
+
+            $jsonModules[$j]['ufs'] = $jsonUfs;
+        }
+
+        $courseInfo = array("year" => $i ,"modules" => $jsonModules);
+        array_push($jsonFinal,$courseInfo);
     }
-    //jsonModules=(array)$jsonModules;
-    $jsonModules[0]['ufs']=$jsonUfs;
-    //$jsonModules=(object)$jsonModules;
 
-  //  $courseInfo1 = array("year" => 1 ,"modules" => array_merge($jsonModules->toArray(),$jsonUfs->toArray()));        
-  //  $courseInfo2 = array("year" => 2 ,"modules" => array_merge($jsonModules->toArray(),$jsonUfs->toArray()));    
-    
-  //  $courseFinale = array($courseInfo1,$courseInfo2);
-  //  $jsonFinal = json_encode($courseFinale);
+    // $jsonFinal = json_encode($jsonFinal);
 
-    return $jsonModules;
+    return $jsonFinal;
 }]);
 
 Route::post('students/find', [ApiController::class, 'searchStudent']);
