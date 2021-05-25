794373e346a2996bf21e4cb43ec608f1fa7b24ac
AngelMiguel2020 <73338230+AngelMiguel2020@users.noreply.github.com>
2021-05-25 09:46:22 +0200

    Custodian selector

--- backend/resources/views/admin/custodian/form.blade.php
+++ backend/resources/views/admin/custodian/form.blade.php
@@ -1,12 +1,28 @@
+<?php
+
+use App\Models\Student; ?>
 <div class="grid grid-cols-2 gap-6">
     <div class="grid grid-rows-2 gap-6">
         <div>
-            <x-label class="required" for="id_student" :value="__('Iid del alumno')" />
-            <x-input id="id_student" type="text" name="id_student" placeholder="" value="{{ $custodian->name }}" class="block mt-1 w-full" autofocus />
+            <x-label class="required" for="id_student" :value="__('Alumno')" />
+            <select id="type" name="id_student" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                <?php
+                $students = Student::all();
+                foreach ($students as $student) {
+                    if ($student->id == $custodian->id_student) {
+                        echo "<option selected value='$student->id'>$student->name</option>";
+                    } else {
+                        echo "<option value='$student->id'>$student->name</option>";
+                    }
+                }
+                ?>
+            </select>
+
         </div>
         <div>
             <x-label for="nif" :value="__('DNI del responsable')" />
             <x-input id="nif" type="text" name="nif" placeholder="" value="{{ $custodian->nif }}" class="block mt-1 w-full" autofocus />
+
         </div>
         <div>
             <x-label for="last_name1" :value="__('Primer apellido delresponsable')" />
