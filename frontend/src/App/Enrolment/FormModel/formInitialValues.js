66c150a608b881c691f33cdbeaa63c9bda74ceb8
a18jorcalari <a18jorcalari@inspedralbes.cat>
2021-05-27 10:09:35 +0200

    pequeños cambios

--- frontend/src/App/Enrolment/FormModel/formInitialValues.js
+++ frontend/src/App/Enrolment/FormModel/formInitialValues.js
@@ -8,35 +8,10 @@ let formInitialValues = {
     email: "",
     date_birth: "",
   },
-  // custodian: {
-  //   custodian: "",
-  //   name_lastname: "",
-  //   nif: "",
-  //   mobile: "",
-  //   email: "",
-  // },
   custodians: [],
   academic_data: {
     course: "",
-    moduluf: [
-      // { mp1: [] },
-      // { mp2: [] },
-      // { mp3: [] },
-      // { mp4: [] },
-      // { mp5: [] }, //
-      // { mp6: [] }, //
-      // { mp7: [] },
-      // { mp8: [] }, //
-      // { mp9: [] }, //
-      // { mp10: [] },
-      // { mp11: [] }, //
-      // { mp12: [] },
-      // { mp13: [] },
-      // { mp14: [] }, //
-      // { mp15: [] }, //
-      // { mp16: [] }, //
-      // { mp17: [] }, //
-    ],
+    moduluf: [],
   },
 };
 
--- frontend/src/App/Menu/Menu.jsx
+++ frontend/src/App/Menu/Menu.jsx
@@ -4,13 +4,7 @@ import EnrolmentList from "./EnrolmentList";
  * Componente que usa Router y crea la lista de ciclos que hay disponibles
  */
 export const Menu = () => {
-  return (
-    // <Router>
-    //   <Route path="/">
-    <EnrolmentList />
-    //   {/* </Route>
-    // </Router> */}
-  );
+  return <EnrolmentList />;
 };
 
 export default Menu;
